16de65302060c625d710253e6d7460c7b820c044
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-01 22:59:48 -0600

    tamaño de la imagen
    se agrego el tamaño de las imágenes

--- TicTacToe/venv/tictactoe.py
+++ TicTacToe/venv/tictactoe.py
@@ -7,3 +7,7 @@ pygame.display.set_caption("Tic Tac Toe")
 fondo = pygame.image.load('static/tictactoe_background.png')
 circulo = pygame.image.load('static/circle.png')
 equis = pygame.image.load('static/x.png')
+
+fondo = pygame.transform.scale(fondo, (450,450))
+circulo = pygame.transform.scale(circulo, (125,125))
+equis = pygame.transform.scale(equis, (125,125))
