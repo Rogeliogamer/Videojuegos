aa7f94d78dca5a0a71d8b88c9427b1b1be7cf059
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-01 23:39:21 -0600

    cambia la pantalla
    cambia la pantalla después de que halla cambios

--- TicTacToe/venv/tictactoe.py
+++ TicTacToe/venv/tictactoe.py
@@ -69,4 +69,6 @@ while not game_over:
                         print(f"El jugador {turno} ha ganado!!")
                         game_over = True
                     turno = 'O' if turno == 'X' else 'X'
+
     graficar_board()
+    pygame.display.update()
