acec95697701d1f39196a239eb9994c77ef786cd
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-02 16:13:45 -0600

    Configuración de ventana
    Se agrego la configuración de la pantalla: titulo, color, tamaño

--- Snake/snake.py
+++ Snake/snake.py
@@ -7,3 +7,10 @@ posponer = 0.1
 #Marcador
 score = 0
 high_score = 0
+
+#Configuracion de la ventana
+screen = turtle.Screen()
+screen.title("Juego de Snake")
+screen.bgcolor("black")
+screen.setup(width = 600, height = 600)
+screen.tracer(0)
