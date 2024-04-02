326aa7bd7bd861d9491203647621ee3e1c7e312c
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-01 22:58:59 -0600

    imagenes
    se guarda en un variable la dirección de la imagen

--- TicTacToe/venv/tictactoe.py
+++ TicTacToe/venv/tictactoe.py
@@ -3,3 +3,7 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((450,450))
 pygame.display.set_caption("Tic Tac Toe")
+
+fondo = pygame.image.load('static/tictactoe_background.png')
+circulo = pygame.image.load('static/circle.png')
+equis = pygame.image.load('static/x.png')
