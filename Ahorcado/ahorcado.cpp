3c7e8a8fef867064832bb89778c34eaa0da912b8
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-13 16:15:40 -0600

    Elementos std
     se utilizarán todos los elementos del espacio de nombres std

--- Ahorcado/ahorcado.cpp
+++ Ahorcado/ahorcado.cpp
@@ -1,3 +1,6 @@
 #include <iostream>
 #include <cstdlib> // Para la función system
 #include <ctime>   // Para la función time
+
+// La siguiente línea indica que se utilizarán todos los elementos del espacio de nombres std
+using namespace std;
