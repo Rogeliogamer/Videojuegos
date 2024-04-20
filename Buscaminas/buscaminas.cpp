6f05d2e2d24c701ca8f51e6f71f068f908c4472b
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-20 14:10:12 -0600

    Variables
    Se agregaron las variables

--- Buscaminas/buscaminas.cpp
+++ Buscaminas/buscaminas.cpp
@@ -14,3 +14,17 @@ void VaciarSector(int y, int x);
 
 //Matriz
 vector<vector<int>> tablero;
+
+//Variables
+//Eleccion del menu
+char eleccion;
+//Tamaño del tablero
+vector<int> tamTablero(2);
+//Posición en el tablero
+vector<int> posCasilla(2);
+//Cantidad de minas
+int cantMinas = 0;
+//Tablero con bombas
+bool tableroConBombas = false;
+//Vecinos bombas
+vector<vector<int>> operaciones = {{1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}};
