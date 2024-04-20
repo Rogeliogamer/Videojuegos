bf78ee8e17deedc75290084df483f22f52e9f69d
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-20 14:08:58 -0600

    Funciones
    Se agregaron las funciones

--- Buscaminas/buscaminas.cpp
+++ Buscaminas/buscaminas.cpp
@@ -3,3 +3,11 @@
 #include <time.h>
 
 //Funciones
+void LimpiarPantalla();
+void Configuracion();
+void JugarPartida();
+bool ComprobarCasillas();
+void MostrarTablero(bool mostrarBombas);
+int DescubrirCasilla();
+void LlenarTablero();
+void VaciarSector(int y, int x);
